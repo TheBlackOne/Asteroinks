6607b08efd86d4fe5213bec3982cd0b0770a2237
Carsten Holbach <carsten@eutopiastudios.net>
2023-05-09 21:24:35 +0200

    Added Pig script (might be renamed later)

---
+++ Assets/Scripts/Pig.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    public float maxVelocity;
+    public float maxAngularVelocity;
+    public int poolIndex;
+    private Rigidbody2D rb;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Reset()
+    {
+        var newVelocity = new Vector2(Random.Range(-maxVelocity, maxVelocity), Random.Range(-maxVelocity, maxVelocity));
+
+        Debug.LogFormat("Setting velocity to {0}", newVelocity);
+        rb.velocity = newVelocity;
+
+        float newAngularVelocity = Random.Range(-maxAngularVelocity, maxAngularVelocity);
+        Debug.LogFormat("Setting angular velocity to {0}", newAngularVelocity);
+        rb.angularVelocity = newAngularVelocity;
+    }
+}
