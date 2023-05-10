7605e0901429ee02d7dd0f7ae3ff1c97584ed6fa
Carsten Holbach <carsten@eutopiastudios.net>
2023-05-10 17:53:15 +0200

    Removed BirdController component, added ShipController first iteration

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float thrust;
+    public float maxThrust;
+    public float turnRate;
+    private Rigidbody2D rigidBody;
+
+    public void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddRelativeForce(new Vector2(0, thrust), ForceMode2D.Force);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 rotationToAdd = new Vector3(0, 0, turnRate);
+            transform.Rotate(rotationToAdd);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 rotationToAdd = new Vector3(0, 0, -turnRate);
+            transform.Rotate(rotationToAdd);
+        }
+    }
+}
