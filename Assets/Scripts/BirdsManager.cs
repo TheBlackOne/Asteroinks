fa6750c65d1abb8d862243979c04702752ad0091
Carsten Holbach <carsten@eutopiastudios.net>
2023-05-12 11:44:49 +0200

    Added object pool for birds, also SpawnBird method

--- Assets/Scripts/BirdsManager.cs
+++ Assets/Scripts/BirdsManager.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class BirdsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject birdPrefab;
+    public int maxNumBirds;
+    private ObjectPool<GameObject> birdObjectPool;
+
+
+    private void CreateObjectPool()
     {
-        
+        birdObjectPool = new ObjectPool<GameObject>(
+            createFunc: () => Instantiate(birdPrefab),
+            actionOnGet: (obj) => obj.SetActive(true), 
+            actionOnRelease: (obj) => obj.SetActive(false), 
+            actionOnDestroy: (obj) => Destroy(obj), 
+            collectionCheck: false, 
+            defaultCapacity: maxNumBirds, 
+            maxSize: maxNumBirds);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnBird(Vector2 position, Vector3 direction)
     {
-        
+        var newBird = birdObjectPool.Get();
+        newBird.GetComponent<BirdController>().Reset(position, direction);
+
+    }
+
+    public void Awake()
+    {
+        CreateObjectPool();
     }
 }
