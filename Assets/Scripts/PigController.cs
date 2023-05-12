80b1730ade6c7a1f40129103e832094eb99eb4f6
Carsten Holbach <carsten@eutopiastudios.net>
2023-05-12 09:47:44 +0200

    Pigs are spawned with a random initial rotation now

--- Assets/Scripts/PigController.cs
+++ Assets/Scripts/PigController.cs
@@ -19,6 +19,10 @@ public class PigController : MonoBehaviour
         Debug.LogFormat("Setting position to {0}...", position);
         transform.position = position;
 
+        float newRotationDegrees = Random.Range(0f, 360f);
+        Debug.LogFormat("Setting rotation to {0}", newRotationDegrees);
+        transform.Rotate(0.0f, 0.0f, newRotationDegrees);
+
         var newVelocity = new Vector2(Random.Range(-maxVelocity, maxVelocity), Random.Range(-maxVelocity, maxVelocity));
 
         Debug.LogFormat("Setting velocity to {0}", newVelocity);
